ab9cc565290a64983c06265c95d3dec4eef2a1b9
Ryan Ernst <ryan@iernst.net>
2020-08-29 14:17:38 -0700

    git merge conflict icon

--- config/zsh/prompt/80-git-branch.zsh
+++ config/zsh/prompt/80-git-branch.zsh
@@ -1,10 +1,16 @@
 _git_branch_info() {
   #local branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
   local branch=$(git symbolic-ref --short -q HEAD 2>/dev/null)
+  local detached=$?
 
   if [ ! -z "$branch" ]; then
     local branch_len="${#branch}"
     (( len = 2 + branch_len ))
+    git rev-list -1 MERGE_HEAD &> /dev/null
+    if git rev-list -1 MERGE_HEAD &> /dev/null; then
+      branch="$branch $(_bc red ✘)"
+      (( len += 2 ))
+    fi
     echo -n "$len: $branch"
   fi
 }
