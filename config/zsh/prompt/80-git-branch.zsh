17b775e291301a9a74b1f3fe4c619c6347f4783e
Ryan Ernst <ryan@iernst.net>
2020-08-29 15:15:53 -0700

    more git

--- config/zsh/prompt/80-git-branch.zsh
+++ config/zsh/prompt/80-git-branch.zsh
@@ -1,23 +1,32 @@
 _git_branch_info() {
-  #local branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-  local branch=$(git symbolic-ref --short -q HEAD 2>/dev/null)
+  # TODO: consider async for future, as well as using vcs info native in git
+  # https://vincent.bernat.ch/en/blog/2019-zsh-async-vcs-info
+  
+  local branch
+  branch=$(git symbolic-ref --short -q HEAD 2>/dev/null)
   local detached=$?
   if [ $detached -ne 0 ]; then
-    branch=$(git rev-parse --short HEAD)
+    branch=$(git rev-parse --short HEAD 2>/dev/null)
   fi
 
   if [ ! -z "$branch" ]; then
     local branch_len="${#branch}"
+    branch=" $branch"
+    local color=
     (( len = 2 + branch_len ))
     git rev-list -1 MERGE_HEAD &> /dev/null
-    if git rev-list -1 MERGE_HEAD &> /dev/null; then
+    if [ $? -eq 0 ]; then
       branch="$branch $(_bc red ✘)"
       (( len += 2 ))
     fi
-    if [ $detached -ne 0 ]; then
-      branch="$(_lc 229 $branch)"
+    local stashes=$(git stash list)
+    if [ ! -z "$stashes" ]; then
+      branch="$branch $(_bc yellow ⚑)"
     fi
-    echo -n "$len: $branch"
+    if [ $detached -ne 0 ]; then
+      branch="$(_lc 227 $branch)"
+    fi
+    echo -n "$len:$branch"
   fi
 }
 
