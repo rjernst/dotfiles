7bb90057151670d8f564fc9a32bbdca40f850c74
Ryan Ernst <ryan@iernst.net>
2020-08-29 14:24:04 -0700

    git detached head detection

--- config/zsh/prompt/80-git-branch.zsh
+++ config/zsh/prompt/80-git-branch.zsh
@@ -2,6 +2,9 @@ _git_branch_info() {
   #local branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
   local branch=$(git symbolic-ref --short -q HEAD 2>/dev/null)
   local detached=$?
+  if [ $detached -ne 0 ]; then
+    branch=$(git rev-parse --short HEAD)
+  fi
 
   if [ ! -z "$branch" ]; then
     local branch_len="${#branch}"
@@ -10,6 +13,9 @@ _git_branch_info() {
     if git rev-list -1 MERGE_HEAD &> /dev/null; then
       branch="$branch $(_bc red ✘)"
       (( len += 2 ))
+    fi
+    if [ $detached -ne 0 ]; then
+      branch="$(_lc 229 $branch)"
     fi
     echo -n "$len: $branch"
   fi
