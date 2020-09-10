a0adc4e3de13e8b438a2760e65f76bd92a89f763
Ryan Ernst <ryan@iernst.net>
2020-09-09 23:42:03 -0700

    Add script to set capslock to control

---
+++ config/macos/prefs/capslock.sh
@@ -0,0 +1,68 @@
+osascript <<EOD
+
+tell application "System Preferences"
+	activate
+	set current pane to pane "com.apple.preference.keyboard"
+end tell
+
+tell application "System Events"
+	tell process "System Preferences"
+    repeat until exists window "Keyboard"
+    end repeat
+		tell window "Keyboard"
+
+			click button "Modifier Keys…" of tab group 1
+      repeat until exists sheet 1
+      end repeat
+
+      if exists pop up button "Select keyboard:" of sheet 1 then
+				set keyboardDropDown to pop up button "Select keyboard:" of sheet 1
+
+        -- open the drop down to extract the list of keyboard names
+				click keyboardDropDown
+				repeat until exists menu 1 of keyboardDropDown
+				end repeat
+				set keyboardNames to name of menu items in menu 1 of keyboardDropDown
+				key code 53
+
+				repeat with keyboard in keyboardNames
+          -- reopen the drop down before selecting each one
+					click keyboardDropDown
+          repeat until exists menu 1 of keyboardDropDown
+					end repeat
+
+          -- select the keyboard and set the caps lock action
+					click menu item keyboard of menu 1 of keyboardDropDown
+					my setCapsLockToControl()
+				end repeat
+			else
+				-- just one keyboard
+				my setCapsLockToControl()
+			end if
+      
+			click button "OK" of sheet 1
+		end tell
+	end tell
+end tell
+
+quit application "System Preferences"
+
+on setCapsLockToControl()
+  tell application "System Events"
+	  tell process "System Preferences"
+		  tell window "Keyboard"
+
+        -- open the drop down
+        set capsLockDropDown to pop up button "Caps Lock (⇪) Key:" of sheet 1
+        click capsLockDropDown
+        repeat until exists menu 1 of capsLockDropDown
+        end repeat
+
+        click menu item "⌃ Control" of menu 1 of capsLockDropDown
+
+      end tell
+    end tell
+  end tell
+end setCapsLockToControl
+
+EOD
