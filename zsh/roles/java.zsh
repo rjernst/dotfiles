00b7a2704c4d66d6e3521e70a9358c15e2bf9e44
Ryan Ernst <ryan@iernst.net>
2020-11-16 21:59:44 -0800

    rework jenv to produce JAVAX_HOME env vars

--- zsh/roles/java.zsh
+++ zsh/roles/java.zsh
@@ -9,12 +9,26 @@ _init_jenv
 # shortcut to current java home
 alias jdk='readlink $(jenv javahome)'
 
-if [ $PLATFORM = "macos" ]; then
-  local jdk_base=/Library/Java/JavaVirtualMachines
-  local jdk_suffix="Contents/Home"
-fi
+# create a shortcut for setting to each major version
+function _setup_jdk_aliases() {
+  _jdk_major_versions=("${(@f)$(jenv versions | grep -E "((\d+\.0)|(1.8))$" | sed -E 's/.* ([0-9]+)\.0$/\1/' | sed -E 's/.* (1.8)$/\1/' | sort -r --unique)}")
+  for v in $_jdk_major_versions; do
+    major=$v
+    if [ "$v" = '1.8' ]; then
+      major=8
+    fi
+    alias jdk${major}='jenv shell '${v}
+    export JAVA${major}_HOME=$HOME/.jenv/versions/${v}
+  done
+}
+_setup_jdk_aliases
 
 function _reload_jenv() {
+  if [[ $OSTYPE == darwin* ]]; then
+    local jdk_base=/Library/Java/JavaVirtualMachines
+    local jdk_suffix="Contents/Home"
+  fi
+
   rm -rf $HOME/.jenv
   _init_jenv
   for jdk in $jdk_base/*; do
@@ -22,15 +36,11 @@ function _reload_jenv() {
     jenv add $jdk/$jdk_suffix
   done
 
-  # create a shortcut for setting to each major version
-  local major_versions=("${(@f)$(jenv versions | grep -E '\d+' | grep -E -v '^\W+[a-z]' | sed -E 's/.* ([0-9]+)\.0$/\1/' | grep -E -v '\W' | sort -r --unique)}")
-  for v in $major_versions; do
-    alias jdk${v}='jenv shell '${v}
-  done
-
   # set the global to be the newest. note that this won't be an ea release because that
   # is excluded above in the major version  
-  jenv global ${major_versions[1]}
+  jenv global ${_jdk_major_versions[1]}
+
+  _setup_jdk_aliases
 }
 alias reload-jenv='_reload_jenv'
 
