b013a3f88bab84c2abdc3421c94b8a32748f030d
Ryan Ernst <ryan@iernst.net>
2024-03-09 12:08:52 -0800

    Better elasticsearch debugging

--- zsh/roles/elasticsearch-support.zsh
+++ zsh/roles/elasticsearch-support.zsh
@@ -22,6 +22,7 @@ function _set_env() {
   echo "Setting up $ENV_NAME support environment"
   if [[ $ENV_NAME == prod ]]; then
     TSH_PROXY=teleport-proxy.secops.elstc.co
+    ADMIN_UI=https://overview.elastic-cloud.com/
     ENV_URL=https://admin.global.elastic.cloud
     ENV_LINKS="
     Cloud UI:      https://cloud.elastic.co/
@@ -32,6 +33,7 @@ function _set_env() {
   fi
   if [[ $ENV_NAME == staging ]]; then
     TSH_PROXY=teleport-proxy.staging.getin.cloud
+    ADMIN_UI=https://admin.staging.foundit.no
     ENV_URL=https://admin.global.staging.cld.elstc.co
     ENV_LINKS="
     Cloud UI:      https://staging.found.no/
@@ -42,10 +44,11 @@ function _set_env() {
   fi
   if [[ $ENV_NAME == qa ]]; then
     TSH_PROXY=teleport-proxy.staging.getin.cloud
+    ADMIN_UI=https://admin.qa.cld.elstc.co
     ENV_URL=https://admin.global.qa.cld.elstc.co
     ENV_LINKS="
     Cloud UI:      https://console.qa.cld.elstc.co/
-    Admin UI:      https://admin.qa.cld.elstc.co/projects/
+    Admin UI:      https://admin.qa.cld.elstc.co/
     Observability: https://overview.qa.cld.elstc.co/
     Logs:          https://overview.qa.cld.elstc.co/app/discover#/view/df2a5ec0-4e20-11ee-889b-75400a7667fb?_g=h@2294574&_a=h@38de511
     "
@@ -53,13 +56,21 @@ function _set_env() {
 
   API_KEY=$(security find-generic-password -a $USER -s admin-console-api-key-$ENV_NAME -w 2> /dev/null)
   if [ $? -ne 0 ]; then
-    echo "API key is not set for $ENV_NAME. Use 'set-env $ENV_NAME' to set it."
+    echo "API key is not set for $ENV_NAME. Use 'set-api-key $ENV_NAME' to set it."
+    echo "Create a key at $ADMIN_UI/keys"
     return 3
   fi
+  curl -s "${ENV_URL}/api/v1/admin/serverless/projects/elasticsearch" -f -H "Authorization: ApiKey $API_KEY" >/dev/null || echo "fail"
+  if [ $? -ne 0 ]; then
+    echo "API key is not valid for $ENV_NAME. Use 'set-api-key $ENV_NAME' to reset it."
+    echo "Create a key at $ADMIN_UI/keys"
+    return 4
+  fi
+
   ping -c 1 $TSH_PROXY &> /dev/null
   if [ $? -ne 0 ]; then
     echo "Cannot reach tsh proxy at $TSH_PROXY. Are you logged into the VPN?"
-    return 4
+    return 5
   fi
   echo "Logging into tsh at $TSH_PROXY"
   tsh login --proxy=$TSH_PROXY --auth=okta
@@ -81,14 +92,14 @@ function _set_api_key() {
       echo "Expected a single ENV parameter"
   fi
   ENV_NAME=$1
-  security add-generic-password -a $USER -s admin-console-api-key-$ENV_NAME -w
+  security add-generic-password -a $USER -s admin-console-api-key-$ENV_NAME -U -w
 }
 alias set-api-key='_set_api_key'
 
 
 function _set_project() {
-  if [[ ! -v SOME_VARIABLE ]]; then
-    echo "Must first run 'setup-support ENV' before setting project id"
+  if [[ ! -v ENV_NAME ]]; then
+    echo "Must first run 'set-env prod|staging|qa' before setting project id"
     return
   fi
   if [ $# -ne 1 ]; then
@@ -115,3 +126,17 @@ function _set_project() {
   kubectl get pods -n project-$PROJECT_ID
 }
 alias set-project='_set_project'
+
+function _get_build_info() {
+  if [[ ! -v ENV_NAME ]]; then
+    echo "Must first run 'set-env prod|staging|qa' before getting version info"
+    return
+  fi
+  if [[ ! -v PROJECT_ID ]]; then
+    echo "Must first run 'set-project PROJECT_ID' before getting version info"
+    return
+  fi
+
+  curl -s -H "Authorization: ApiKey $API_KEY" -H "Content-Type: application/json" "$ENV_URL/api/v1/admin/serverless/projects/$PROJECT_TYPE/$PROJECT_ID/_proxy/_nodes" | jq -cMr '["Node","Serverless Build Hash"], (.nodes | keys[] as $key | .[$key] | [.name, .build_hash]) | @tsv' | column -t -s $'\t' 
+}
+alias get-build-info='_get_build_info'
